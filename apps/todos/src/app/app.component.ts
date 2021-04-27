b0c0dc6e27e6959f9032448d2b529a0ca61a3aeb
awarrington <awarrington0895@gmail.com>
2021-04-27 00:47:43 +0000

    Add basic frontend

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
 
 @Component({
   selector: 'devops-root',
@@ -6,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos')
+      .subscribe(t => this.todos = t);
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {})
+      .subscribe(() => {
+        this.fetch();
+      });
+  }
 }
