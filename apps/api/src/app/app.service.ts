f74c58280adc280455551aa7670b91dfd1d7859b
awarrington0895 <awarrington0895@gmail.com>
2021-05-02 14:21:09 +0000

    Remove console.log

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -6,7 +6,6 @@ export class AppService {
   todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
 
   getData(): Todo[] {
-    console.log('Someone got some todos');
     return this.todos;
   }
 
